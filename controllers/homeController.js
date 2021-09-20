7a599b584c32ba0fc636bf9276e27d2306c62994
Manik Gupta <manikpokhetra@gmail.com>
2021-09-20 21:36:09 +0530

    created readme

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,5 @@
 module.exports.home = (req, res) => {
-  return res.send('<h2>Polling System Api</h2>');
+  return res.send(`<h2>Polling System Api</h2>
+  <a href="https://github.com/manikpokhetra79/PollingSystem_APi">Vist Gihub Repository to read on How to use this API.</a>
+  `);
 };
